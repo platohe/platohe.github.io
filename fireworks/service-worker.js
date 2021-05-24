3c004a2581e2940499a989394e8f739dfae5ff4c
platohe <hexinsheng@gmail.com>
2021-05-24 19:13:12 -0400

    fireworks build#1.0.210524

--- fireworks/service-worker.js
+++ fireworks/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.272ff0a58ba15ebe1ed478ddfee5bbed.js"
+  "./precache-manifest.69233a2150b28cbcc1cd224f3d9f4e1f.js"
 );
 
 self.addEventListener('message', (event) => {
